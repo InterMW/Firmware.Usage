19abf09d43d3e4aa5d41327aded7f9b87ba4317e
Joseph-Melberg <dasbasdo@gmail.com>
2024-04-21 19:56:24 -0500

    Update script.py

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ def system_call(command):
     p = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
     return p.stdout.read()
  
-def action():
+def action(mac):
     value = float(str(system_call("uptime")).split(" ")[-3][:-1])
     core_count = float(str(system_call("nproc"))[2])
     credentials = pika.PlainCredentials(os.environ["user"],os.environ["pass"])
@@ -20,13 +20,15 @@ def action():
 
     message = {}
 
-    message["HostName"] = socket.gethostname()
+    message["HostName"] = mac
     message["Usage"] = str(value/core_count)
 
     channel.basic_publish(exchange='InterTopic', routing_key='node.usage', body=str(message))
     connection.close()
 
+mac = str(systemcall("$(ifconfig eth0 | grep -Eo ..\(\:..\){5})"))
+
 while(True):
-    action()
+    action(mac)
     sleep(60)
 
