3ef70894669c053442de7fb568bad53f5dda715d
Joseph-Melberg <josephbmelbergthird@outlook.com>
2024-04-22 00:19:36 +0000

    now without stored passwords

---
+++ script.py
@@ -0,0 +1,33 @@
+import os
+import time
+import socket
+import subprocess
+import pika
+import json
+import time
+import sys
+def system_call(command):
+    p = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
+    return p.stdout.read()
+ 
+def action():
+    value = float(str(system_call("uptime")).split(" ")[-3][:-1])
+    core_count = float(str(system_call("nproc"))[2])
+    credentials = pika.PlainCredentials(os.environ["user"],os.environ["pass"])
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbit.centurionx.net',5672,'/',credentials))
+    channel = connection.channel()
+    channel.exchange_declare(exchange='Inter',exchange_type='direct',durable=True)
+
+
+    message = {}
+
+    message["HostName"] = socket.gethostname()
+    message["Usage"] = str(value/core_count)
+
+    channel.basic_publish(exchange='InterTopic', routing_key='node.usage', body=str(message))
+    connection.close()
+
+while(True):
+    action()
+    sleep(60)
+
